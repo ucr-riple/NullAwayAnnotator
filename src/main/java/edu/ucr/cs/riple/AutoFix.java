bdb03ae70a614cea1f75a437460885f4fb6a034c
nimakarimipour <karimipour.nima@gmail.com>
2020-07-13 12:29:07 -0500

    completed phase 1

--- src/main/java/edu/ucr/cs/riple/AutoFix.java
+++ src/main/java/edu/ucr/cs/riple/AutoFix.java
@@ -1,11 +1,28 @@
 package edu.ucr.cs.riple;
 
+import edu.riple.annotationinjector.Fix;
+import edu.riple.annotationinjector.Injector;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.TaskAction;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 
 public class AutoFix extends DefaultTask {
+
+    Injector injector = null;
 
     static void runTask(Project project, String task){
         String command = "cd " + project.getProjectDir().getAbsolutePath() + " && ./gradlew" + " " + task;
@@ -27,9 +44,47 @@ public class AutoFix extends DefaultTask {
             extension = new NullAwayAutoFixExtension();
         }
 
+        //todo: use later
         String mode = extension.getMode();
-        System.out.println("Current MODE is: " + mode);
 
-        runTask(getProject(), "build");
+        String fixPath = extension.getFixPath() == null ? getProject().getProjectDir().getAbsolutePath() : extension.getFixPath();
+        fixPath = (fixPath.endsWith("/") ? fixPath : fixPath + "/") + "fixes.json";
+
+
+        System.out.println("Fix path: " + fixPath);
+        injector = Injector.builder(Injector.MODE.OVERWRITE).setFixesJsonFilePath(fixPath).build();
+        run(fixPath);
+    }
+
+    private void run(String fixPath) {
+        boolean finished = false;
+
+        while (!finished) {
+            runTask(getProject(), "build");
+            if(newFixRequested(fixPath)) {
+                System.out.println("NullAway found some error(s), going for next round...");
+                injector.start();
+                new File(fixPath).delete();
+            }
+            else {
+                System.out.println("NullAway found no errors, shutting down.");
+                finished = true;
+            }
+        }
+    }
+
+    private boolean newFixRequested(String path){
+        try {
+            BufferedReader bufferedReader =
+                    Files.newBufferedReader(Paths.get(path), Charset.defaultCharset());
+            JSONObject obj = (JSONObject) new JSONParser().parse(bufferedReader);
+            JSONArray fixesJson = (JSONArray) obj.get("fixes");
+            bufferedReader.close();
+            return fixesJson.size() > 0;
+        } catch (IOException ex) {
+            return false;
+        } catch (ParseException e) {
+            throw new RuntimeException("Error in parsing object: " + e);
+        }
     }
 }
--- src/main/java/edu/ucr/cs/riple/NullAwayAutoFixExtension.java
+++ src/main/java/edu/ucr/cs/riple/NullAwayAutoFixExtension.java
@@ -3,6 +3,15 @@ package edu.ucr.cs.riple;
 public class NullAwayAutoFixExtension {
 
     private String mode = "overwrite";
+    private String fixPath = null;
+
+    public String getFixPath() {
+        return fixPath;
+    }
+
+    public void setFixPath(String fixPath) {
+        this.fixPath = fixPath;
+    }
 
     public String getMode() {
         return mode;
