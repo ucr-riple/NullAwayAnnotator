94d383f9591a8439b421552d1ef537ef0c9ae57d
nimakarimipour <karimipour.nima@gmail.com>
2020-07-23 13:30:40 -0500

    bug fix

--- src/main/java/edu/ucr/cs/riple/AutoFix.java
+++ src/main/java/edu/ucr/cs/riple/AutoFix.java
@@ -16,94 +16,117 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-
 public class AutoFix extends DefaultTask {
 
-    Injector injector = null;
-    String executable = "gradlew";
+  Injector injector = null;
+  String executable = "gradlew";
 
-    void buildProject(Project project){
-        String task = "build";
-        String setToJava11 = "export JAVA_HOME=`/usr/libexec/java_home -v 11` && ";
-        String hideOutput = "> /dev/null 2>&1";
-        String command = ""
-                + "cd "
-                + project.getProjectDir().getAbsolutePath()
-                + " && ./"
-                + executable + " "
-                + task + " "
-                + "--rerun-tasks "
-                + hideOutput;
+  void buildProject(Project project) {
+    String executablePath = project.getProjectDir().getAbsolutePath();
+    String subProjectPath = project.getPath().replace(":", "");
+    if (executablePath.endsWith(subProjectPath)) {
+      executablePath = executablePath.replace(subProjectPath, "");
+    }
+    String task = "build";
+    String setToJava8 = javaEnvironmentCommand("1.8");
+    String hideOutput = "> /dev/null 2>&1";
+    String command =
+        ""
+            + setToJava8
+            + " && "
+            + "cd "
+            + executablePath
+            + " && ./"
+            + executable
+            + " "
+            + task
+            + " "
+            + "--rerun-tasks"
+            + hideOutput;
 
-        System.out.println("Running: " + command);
+    System.out.println("Running: " + command);
 
-        Process proc;
-        try {
-            proc = Runtime.getRuntime().exec(new String[] { "/bin/sh", "-c", command });
-            if (proc != null) {
-                proc.waitFor();
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("Could not run command: " + task + " from gradle");
-        }
+    Process proc;
+    try {
+      proc = Runtime.getRuntime().exec(new String[] {"/bin/sh", "-c", command});
+      if (proc != null) {
+        proc.waitFor();
+      }
+    } catch (Exception e) {
+      throw new RuntimeException("Could not run command: " + task + " from gradle");
+    }
+  }
+
+  private String javaEnvironmentCommand(String version) {
+    return "export JAVA_HOME=`/usr/libexec/java_home -v " + version + "`";
+  }
+
+  @TaskAction
+  public void autoFix() {
+    NullAwayAutoFixExtension extension =
+        getProject().getExtensions().findByType(NullAwayAutoFixExtension.class);
+    if (extension == null) {
+      extension = new NullAwayAutoFixExtension();
     }
 
-    @TaskAction
-    public void autoFix() {
-        NullAwayAutoFixExtension extension = getProject().getExtensions().findByType(NullAwayAutoFixExtension.class);
-        if (extension == null) {
-            extension = new NullAwayAutoFixExtension();
-        }
+    // todo: use later
+    String mode = extension.getMode();
 
-        //todo: use later
-        String mode = extension.getMode();
+    executable = extension.getExecutable();
+    String fixPath =
+        extension.getFixPath() == null
+            ? getProject().getProjectDir().getAbsolutePath()
+            : extension.getFixPath();
+    fixPath = (fixPath.endsWith("/") ? fixPath : fixPath + "/") + "fixes.json";
 
-        executable = extension.getExecutable();
-        String fixPath = extension.getFixPath() == null ? getProject().getProjectDir().getAbsolutePath() : extension.getFixPath();
-        fixPath = (fixPath.endsWith("/") ? fixPath : fixPath + "/") + "fixes.json";
+    System.out.println(
+        "\n"
+            + "=========="
+            + "\n"
+            + "Process info:"
+            + "\n"
+            + "Mode: "
+            + mode
+            + "\n"
+            + "Executable: "
+            + executable
+            + "\n"
+            + "Fix path: "
+            + fixPath);
+    injector = Injector.builder(Injector.MODE.OVERWRITE).setFixesJsonFilePath(fixPath).build();
+    run(fixPath);
+  }
 
-        System.out.println("\n" +
-                "==========" + "\n" +
-                "Process info:" + "\n" +
-                "Mode: " + mode + "\n" +
-                "Executable: " + executable + "\n" +
-                "Fix path: " + fixPath
-        );
-        injector = Injector.builder(Injector.MODE.OVERWRITE).setFixesJsonFilePath(fixPath).build();
-        run(fixPath);
+  private void run(String fixPath) {
+    boolean finished = false;
+    int round = 0;
+
+    while (!finished) {
+      System.out.println("Cleared fix path for new run: " + new File(fixPath).delete());
+      System.out.println("Round " + (++round) + "...");
+      buildProject(getProject());
+      if (newFixRequested(fixPath)) {
+        System.out.println("NullAway found some fixable error(s), going for next round...");
+        injector.start();
+      } else {
+        System.out.println("NullAway found no fixable errors, shutting down.");
+        finished = true;
+      }
     }
+  }
 
-    private void run(String fixPath) {
-        boolean finished = false;
-        int round = 0;
-
-        while (!finished) {
-            System.out.println("Cleared fix path for new run: " + new File(fixPath).delete());
-            System.out.println("Round " + (++round) + "...");
-            buildProject(getProject());
-            if(newFixRequested(fixPath)) {
-                System.out.println("NullAway found some fixable error(s), going for next round...");
-                injector.start();
-            }
-            else {
-                System.out.println("NullAway found no fixable errors, shutting down.");
-                finished = true;
-            }
-        }
+  private boolean newFixRequested(String path) {
+    try {
+      BufferedReader bufferedReader =
+          Files.newBufferedReader(Paths.get(path), Charset.defaultCharset());
+      JSONObject obj = (JSONObject) new JSONParser().parse(bufferedReader);
+      JSONArray fixesJson = (JSONArray) obj.get("fixes");
+      bufferedReader.close();
+      return fixesJson.size() > 0;
+    } catch (IOException ex) {
+      return false;
+    } catch (ParseException e) {
+      throw new RuntimeException("Error in parsing object: " + e);
     }
-
-    private boolean newFixRequested(String path){
-        try {
-            BufferedReader bufferedReader =
-                    Files.newBufferedReader(Paths.get(path), Charset.defaultCharset());
-            JSONObject obj = (JSONObject) new JSONParser().parse(bufferedReader);
-            JSONArray fixesJson = (JSONArray) obj.get("fixes");
-            bufferedReader.close();
-            return fixesJson.size() > 0;
-        } catch (IOException ex) {
-            return false;
-        } catch (ParseException e) {
-            throw new RuntimeException("Error in parsing object: " + e);
-        }
-    }
+  }
 }
