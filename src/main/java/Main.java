d479ff98bb6af22003dd8ac5917286621949fdf3
nimakarimipour <karimipour.nima@gmail.com>
2021-03-29 20:32:34 -0700

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,19 @@
-public class Main {
+import edu.ucr.cs.riple.diagnose.DiagnoseJar;
 
+public class Main {
+  public static void main(String[] args) {
+    //    DiagnoseJar diagnose = new DiagnoseJar();
+    //    if(args.length != 2){
+    //        throw new RuntimeException("Needs exactly two arguments: 1. command to execute
+    // NullAway, 2. path to the suggested fix file");
+    //    }
+    //    String fixPath = args[0];
+    //    String runCommand = args[1];
+    //
+    //    String dir = fixPath.contains("/") ? fixPath.substring(0, fixPath.lastIndexOf("/")) : "/";
+    //    String diagnosePath = dir + "/diagnose.json";
+    //    diagnose.start(runCommand, fixPath, diagnosePath);
+
+    System.out.println("THIS IS A TEST");
+  }
 }
