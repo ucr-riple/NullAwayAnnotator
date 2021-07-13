561b221dee326098cd381128e5c84f62c9b0877c
nimakarimipour <karimipour.nima@gmail.com>
2021-07-13 01:22:04 -0700

    update

--- src/main/java/edu/ucr/cs/riple/autofixer/explorers/ClassFieldExplorer.java
+++ src/main/java/edu/ucr/cs/riple/autofixer/explorers/ClassFieldExplorer.java
@@ -13,7 +13,7 @@ import java.io.File;
 
 public class ClassFieldExplorer extends Explorer {
 
-    private FieldGraph fieldGraph;
+    private final FieldGraph fieldGraph;
 
     public ClassFieldExplorer(Diagnose diagnose, Bank bank) {
         super(diagnose, bank);
