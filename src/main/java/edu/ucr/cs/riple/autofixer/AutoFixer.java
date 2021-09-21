eea8ebb8680fc005f486763135106d88535663e8
nimakarimipour <karimipour.nima@gmail.com>
2021-09-21 15:33:06 -0700

    update

--- src/main/java/edu/ucr/cs/riple/autofixer/AutoFixer.java
+++ src/main/java/edu/ucr/cs/riple/autofixer/AutoFixer.java
@@ -22,12 +22,13 @@ import edu.ucr.cs.riple.injector.Fix;
 import edu.ucr.cs.riple.injector.Injector;
 import edu.ucr.cs.riple.injector.WorkList;
 import edu.ucr.cs.riple.injector.WorkListBuilder;
-import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -51,7 +52,7 @@ public class AutoFixer {
   public FieldUsageTracker fieldUsageTracker;
   public MethodInheritanceTree methodInheritanceTree;
 
-  private void init(String buildCommand) {
+  private List<Fix> init(String buildCommand) {
     this.buildCommand = buildCommand;
     this.fixPath = out_dir + "/fixes.csv";
     this.diagnosePath = out_dir + "/diagnose.json";
@@ -61,13 +62,12 @@ public class AutoFixer {
             .setLogError(true, true)
             .setMakeCallGraph(true)
             .setMakeFieldGraph(true)
-            .setOptimized(true)
             .setMethodInheritanceTree(true)
             .setSuggest(true, true)
             .setAnnots(AutoFixer.NULLABLE_ANNOT, "UNKNOWN")
             .setWorkList(Collections.singleton("*"));
     buildProject(config);
-    List<Fix> allFixes = Utility.readAllFixes();
+    List<Fix> allFixes = Collections.unmodifiableList(Utility.readAllFixes());
     this.injector = Injector.builder().setMode(Injector.MODE.BATCH).build();
     this.methodInheritanceTree = new MethodInheritanceTree(Writer.METHOD_INFO);
     this.callUsageTracker = new CallUsageTracker(Writer.CALL_GRAPH);
@@ -80,15 +80,17 @@ public class AutoFixer {
     this.explorers.add(new ClassFieldExplorer(this, allFixes, errorBank, fixIndex));
     this.explorers.add(new MethodReturnExplorer(this, allFixes, errorBank, fixIndex));
     this.explorers.add(new BasicExplorer(this, errorBank, fixIndex));
+    return allFixes;
   }
 
-  public void start(String buildCommand, String out_dir, boolean optimized) {
+  public void start(String buildCommand, String out_dir, boolean useCache) {
     System.out.println("AutoFixer Started...");
     this.out_dir = out_dir;
-    init(buildCommand);
-    System.out.println("Starting preparation");
-    prepare(out_dir, optimized);
-    List<WorkList> workListLists = new WorkListBuilder(diagnosePath).getWorkLists();
+    List<Fix> fixes = init(buildCommand);
+    if (useCache) {
+      removeCachedFixes(fixes, out_dir);
+    }
+    List<WorkList> workListLists = new WorkListBuilder(fixes).getWorkLists();
     try {
       for (WorkList workList : workListLists) {
         for (Fix fix : workList.getFixes()) {
@@ -154,63 +156,27 @@ public class AutoFixer {
     injector.start(new WorkListBuilder(fixes).getWorkLists(), true);
   }
 
-  @SuppressWarnings("ALL")
-  private void prepare(String out_dir, boolean optimized) {
+
+  private static void removeCachedFixes(List<Fix> fixes, String out_dir) {
+    if (!Files.exists(Paths.get(out_dir + "/reports.json"))) {
+      return;
+    }
     try {
-      System.out.println("Preparing project: with optimization flag:" + optimized);
-      AutoFixConfig.AutoFixConfigWriter config =
-          new AutoFixConfig.AutoFixConfigWriter()
-              .setLogError(true, false)
-              .setMakeCallGraph(false)
-              .setMakeFieldGraph(false)
-              .setOptimized(false)
-              .setMethodInheritanceTree(false)
-              .setSuggest(true, false)
-              .setAnnots(AutoFixer.NULLABLE_ANNOT, "UNKNOWN")
-              .setWorkList(Collections.singleton("*"));
-      buildProject(config);
-      if (!new File(fixPath).exists()) {
-        JSONObject toDiagnose = new JSONObject();
-        toDiagnose.put("fixes", new JSONArray());
-        FileWriter writer = new FileWriter(diagnosePath);
-        writer.write(toDiagnose.toJSONString());
-        writer.flush();
-        System.out.println("No new fixes from NullAway, created empty list.");
-        return;
-      }
-      new File(diagnosePath).delete();
-      convertCSVToJSON(this.fixPath, out_dir + "/fixes.json");
-      System.out.println("Deleted old diagnose file.");
-      System.out.println("Making new diagnose.json.");
-      if (!optimized) {
-        executeCommand("cp " + this.fixPath + " " + this.diagnosePath);
-        convertCSVToJSON(diagnosePath, diagnosePath);
-      } else {
-        try {
-          System.out.println("Removing already diagnosed fixes...");
-          Object obj = new JSONParser().parse(new FileReader(out_dir + "/fixes.json"));
-          JSONObject fixes = (JSONObject) obj;
-          obj = new JSONParser().parse(new FileReader(out_dir + "/diagnosed.json"));
-          JSONObject diagnosed = (JSONObject) obj;
-          JSONArray fixes_array = (JSONArray) fixes.get("fixes");
-          JSONArray diagnosed_array = (JSONArray) diagnosed.get("fixes");
-          fixes_array.removeAll(diagnosed_array);
-          JSONObject toDiagnose = new JSONObject();
-          toDiagnose.put("fixes", fixes_array);
-          FileWriter writer = new FileWriter(diagnosePath);
-          writer.write(toDiagnose.toJSONString());
-          writer.flush();
-        } catch (RuntimeException exception) {
-          System.out.println("Exception happened while optimizing suggested fixes.");
-          System.out.println("Continuing...");
-          executeCommand("cp " + fixPath + " " + diagnosePath);
+      System.out.println("Reading cached fixes reports");
+      JSONObject cachedObjects =
+          (JSONObject) new JSONParser().parse(new FileReader(out_dir + "/reports.json"));
+      JSONArray cachedJson = (JSONArray) cachedObjects.get("fixes");
+      List<Report> cached = new ArrayList<>();
+      for (Object o : cachedJson) {
+        JSONObject reportJson = (JSONObject) o;
+        int effect = Integer.parseInt(reportJson.get("effect").toString());
+        if(effect < 3){
+          cached.add(new Report(Fix.createFromJson(reportJson), effect));
         }
       }
-      System.out.println("Made.");
-      System.out.println("Preparation done");
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+      fixes.removeAll(cached.stream().map(report -> report.fix).collect(Collectors.toList()));
+    } catch (Exception ignored) { }
+    System.out.println("Processing cache fixes finished.");
   }
 
   public void buildProject(AutoFixConfig.AutoFixConfigWriter writer) {
