0f4a50bd048f11f7308a986ca324902511618099
nimakarimipour <karimipour.nima@gmail.com>
2022-03-08 15:51:54 -0800

    Worked on Pre phase

--- runner/run.py
+++ runner/run.py
@@ -6,14 +6,14 @@ import time
 import tools
 import xmltodict
 
-if not (len(sys.argv) in [2, 3]):
-    raise ValueError("Needs one argument to run: diagnose/apply/pre/loop/clean")
+# if not (len(sys.argv) in [2, 3]):
+#     raise ValueError("Needs one argument to run: diagnose/apply/pre/loop/clean")
 
-data = None
-if int(len(sys.argv)) == 2:
-    data = json.load(open('config.json'))
-else:
-    data = json.load(open(sys.argv[2]))
+data = json.load(open('config.json'))
+# if int(len(sys.argv)) == 2:
+#     data = json.load(open('config.json'))
+# else:
+#     data = json.load(open(sys.argv[2]))
 
 if 'REPO_ROOT_PATH' not in data:
     # By default, the path to the repo root (where the build command must be run) and the
@@ -80,33 +80,46 @@ def prepare():
 
 def pre():
     uprint("Started preprocessing task...")
-    method_path = out_dir + "/method_info.csv"
+    method_path = out_dir + "/field_init.tsv"
     delete(method_path)
     delete(out_dir + "/init_methods.json")
     uprint("Building project...\n" + build_command)
     new_config = EXPLORER_CONFIG.copy()
     new_config['serialization']['annotation']['nullable'] = data['ANNOTATION']['NULLABLE']
     new_config['serialization']['annotation']['nonnull'] = data['ANNOTATION']['NONNULL']
+    new_config['serialization']['fieldInitInfo']['@active'] = "true"
     tools.write_dict_config_in_xml(new_config, '/tmp/NullAwayFix/config.xml')
     os.system(build_command + " > /dev/null 2>&1")
     uprint("Analyzing suggested fixes...")
-    fixes = load_csv_to_dict(out_dir + "/fixes.csv")
+    fixes = load_csv_to_dict(out_dir + "/fixes.tsv")
     uprint("Detecting uninitialized class fields...")
-    field_no_inits = [x for x in fixes if (x['reason'] == 'FIELD_NO_INIT' and x['location'] == 'CLASS_FIELD')]
+    field_no_inits = [x for x in fixes if (x['reason'] == 'FIELD_NO_INIT' and x['location'] == 'FIELD')]
     uprint("Found " + str(len(field_no_inits)) + " fields.")
     uprint("Analyzing method infos...")
-    methods = load_csv_to_dict(method_path)
+    raw_methods = load_csv_to_dict(method_path)
     init_methods = {"fixes": []}
+    methods = []
+    for method in raw_methods:
+        seen = None
+        for discovered in methods:
+            if discovered['method'] == method['method'] and discovered['class'] == method['method']:
+                seen = discovered
+                break
+        if seen is None:
+            method['fields'] = []
+            seen = method
+            methods.append(seen)
+        seen['fields'].append(method['field'])
     uprint("Selecting appropriate method for each class field...")
     for field in field_no_inits:
         candidate_method = None
         max = 0
         for method in methods:
-            if (method['class'] == field['class']):
-                if (field['param'] in method['fields'] and len(method['fields']) > max):
+            if method['class'] == field['class']:
+                if field['param'] in method['fields'] and len(method['fields']) > max:
                     candidate_method = method.copy()
                     max = len(method['fields'])
-        if (candidate_method != None):
+        if candidate_method is not None:
             del candidate_method['fields']
             candidate_method['location'] = "METHOD_RETURN"
             candidate_method['inject'] = True
@@ -177,8 +190,10 @@ def loop():
     clean(full=False)
 
 
-command = sys.argv[1]
+# command = sys.argv[1]
 prepare()
+pre()
+exit()
 if command == "pre":
     pre()
 elif command == "diagnose":
