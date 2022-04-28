2349b2f03e618eb4eed26244eb1db0d13249c1db
nimakarimipour <karimipour.nima@gmail.com>
2022-04-27 17:27:14 -0700

    updated help

--- runner/start.sh
+++ runner/start.sh
@@ -1,1 +1,12 @@
-cd jars && java -jar core.jar --path /Users/nima/Developer/NullAwayFixer/NullAwayAnnotator/runner/config.json
+DIR=jars
+if [ ! -d "$DIR" ]; then
+    echo "$DIR does not exists, creating..."
+    mkdir jars
+fi
+FILE=./jars/core.jar
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exists, creating..."
+    ./update.sh
+fi
+
+cd jars && java -jar core.jar "$@"
