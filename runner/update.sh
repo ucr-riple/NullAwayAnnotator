5d7b56db29b13cbdae76eb7b0a82e87cbf17e14b
nimakarimipour <karimipour.nima@gmail.com>
2022-06-10 11:32:42 -0700

    updated update.sh

--- runner/update.sh
+++ runner/update.sh
@@ -1,2 +1,2 @@
-cd ../ && ./gradlew install --rerun-tasks && ./gradlew :core:fatJar --rerun-tasks
+cd ../ && ./gradlew ./gradlew :css:publishToMavenLocal --rerun-tasks && ./gradlew :core:shadowJar --rerun-tasks
 mv core/build/libs/core.jar runner/jars/core.jar
