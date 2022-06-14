4e8284dc10f6f0d20da441e2f1e60b2b882f7dc8
nimakarimipour <karimipour.nima@gmail.com>
2022-06-14 15:34:57 -0700

    Prepare next development version.

--- runner/update.sh
+++ runner/update.sh
@@ -1,2 +1,2 @@
 cd ../ && ./gradlew :css:publishToMavenLocal --rerun-tasks && ./gradlew :core:shadowJar --rerun-tasks
-mv core/build/libs/core-1.3.0-SNAPSHOT-all.jar runner/jars/core.jar
+mv core/build/libs/core-1.3.1-SNAPSHOT-all.jar runner/jars/core.jar
