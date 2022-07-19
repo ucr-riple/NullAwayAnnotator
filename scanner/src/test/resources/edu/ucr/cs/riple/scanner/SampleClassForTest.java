2977561248bebbfeddb2e8a8a66822f48761aa5c
Nima Karimipour <nimak@uber.com>
2022-07-18 17:10:00 -0700

    enhance class test

--- scanner/src/test/resources/edu/ucr/cs/riple/scanner/SampleClassForTest.java
+++ scanner/src/test/resources/edu/ucr/cs/riple/scanner/SampleClassForTest.java
@@ -4,9 +4,23 @@ public class SampleClassForTest {
 
   Object field;
 
-  public void foo() {}
+  static class Inner {}
+
+  public void foo() {
+    class InnerMethod {
+      Run r =
+          new Run() {
+            @Override
+            public void exec() {}
+          };
+    }
+  }
 
   public Object bar() {
     return new Object();
   }
+}
+
+interface Run {
+  void exec();
 }
