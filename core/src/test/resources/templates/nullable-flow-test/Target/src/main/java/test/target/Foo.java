cc0875d11cefe5d6328849c773c58ca164285987
Nima Karimipour <karimipour.nima@gmail.com>
2022-09-01 20:41:17 -0700

    update test src comment

--- core/src/test/resources/templates/nullable-flow-test/Target/src/main/java/test/target/Foo.java
+++ core/src/test/resources/templates/nullable-flow-test/Target/src/main/java/test/target/Foo.java
@@ -25,7 +25,7 @@ package test.target;
 public class Foo {
   Object field = new Object();
 
-  // Making this method will resolve 5 errors here and will trigger 3 errors on downstream
+  // Making this method @Nullable will resolve 5 errors here and will trigger 3 errors on downstream
   // dependencies.
   public Object returnNullable(int i) {
     // Just to create 5 places where it returns nullable.
