44afe2315794b06a7bfdeae3e80ac7a2091d48a7
nimakarimipour <karimipour.nima@gmail.com>
2022-09-16 21:05:55 -0700

    resolve #71

--- core/src/main/java/edu/ucr/cs/riple/core/Annotator.java
+++ core/src/main/java/edu/ucr/cs/riple/core/Annotator.java
@@ -48,6 +48,7 @@ import edu.ucr.cs.riple.injector.changes.AddAnnotation;
 import edu.ucr.cs.riple.injector.changes.AddMarkerAnnotation;
 import edu.ucr.cs.riple.injector.changes.AddSingleElementAnnotation;
 import edu.ucr.cs.riple.injector.location.OnField;
+import edu.ucr.cs.riple.injector.location.OnMethod;
 import edu.ucr.cs.riple.scanner.Serializer;
 import java.util.List;
 import java.util.Objects;
@@ -256,14 +257,27 @@ public class Annotator {
     // the method level.
     Set<AddAnnotation> nullUnMarkedAnnotations =
         remainingErrors.stream()
-            // filter non-method regions.
-            .filter(error -> !error.encMethod().equals("null"))
             // find the corresponding method nodes.
-            .map(error -> tree.findNode(error.encMethod(), error.encClass()))
-            // impossible, just sanity check or future nullness checker hints
+            .map(
+                error -> {
+                  if (!error.encMethod().equals("null")) {
+                    return tree.findNode(error.encMethod(), error.encClass());
+                  }
+                  if (error.nonnullTarget == null) {
+                    // Just a sanity check.
+                    return null;
+                  }
+                  if (error.messageType.equals("PASS_NULLABLE")) {
+                    OnMethod calledMethod = error.nonnullTarget.toMethod();
+                    return tree.findNode(calledMethod.method, calledMethod.clazz);
+                  }
+                  return null;
+                })
             .filter(Objects::nonNull)
             .map(node -> new AddMarkerAnnotation(node.location, config.nullUnMarkedAnnotation))
             .collect(Collectors.toSet());
+    // Collect methods received a nullable i their nonnull parameters where called in field
+    // declaration regions of classes (method == "null).
     injector.injectAnnotations(nullUnMarkedAnnotations);
 
     // Collect explicit Nullable initialization to fields
@@ -299,6 +313,7 @@ public class Annotator {
             // Exclude already annotated fields with a general NullAway suppress warning.
             .filter(f -> !suppressWarningsAnnotations.contains(f))
             .collect(Collectors.toSet());
+
     injector.injectAnnotations(initializationSuppressWarningsAnnotations);
   }
 }
