b30082406d277b8c5da004245cbb2529562e66f5
nimakarimipour <karimipour.nima@gmail.com>
2022-09-23 15:55:11 -0700

    reformat

--- injector/src/test/java/edu/ucr/cs/riple/injector/AnnotationWithArgumentTest.java
+++ injector/src/test/java/edu/ucr/cs/riple/injector/AnnotationWithArgumentTest.java
@@ -307,7 +307,7 @@ public class AnnotationWithArgumentTest extends BaseInjectorTest {
             "   Object h = new Object();",
             "   @CustomNull({\"arg1\", \"arg2\"})",
             "@CustomNull(\"arg3\")", // this is intentional, just to pass unit tests, this can be
-                                     // fixes by google java format.
+            // fixes by google java format.
             "   public void test(Object f) {",
             "      h = f;",
             "   }",
