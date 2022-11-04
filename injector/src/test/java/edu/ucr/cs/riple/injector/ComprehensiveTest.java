797722730b2c57ca200a48c741f18a4f121bd1d6
nimakarimipour <karimipour.nima@gmail.com>
2022-11-04 16:21:32 -0700

    add new unit test

--- injector/src/test/java/edu/ucr/cs/riple/injector/ComprehensiveTest.java
+++ injector/src/test/java/edu/ucr/cs/riple/injector/ComprehensiveTest.java
@@ -206,4 +206,89 @@ public class ComprehensiveTest extends BaseInjectorTest {
                 annot))
         .start();
   }
+
+  @Test
+  public void mixFieldMethodParamTest() {
+    String annot = "javax.annotation.Nullable";
+    injectorTestHelper
+        .addInput(
+            "Test.java",
+            "package edu.ucr;",
+            "import javax.annotation.Nullable;",
+            "public class Test {",
+            "   Field foo1;",
+            "   @Nullable",
+            "   Field foo2;",
+            "   Field foo3, foo4;",
+            "   @Nullable",
+            "   Object bar1(@Nullable Object p1, Object p2) {",
+            "       return new Object();",
+            "   }",
+            "   Object bar2(Object p1, Object p2) {",
+            "       return new Object();",
+            "   }",
+            "   @Nullable",
+            "   Field foo5;",
+            "   Object bar3(Object p1, @Nullable Object p2, Object p3, Object p4) {",
+            "       return new Object();",
+            "   }",
+            "   @Nullable",
+            "   Object bar4() {",
+            "       return new Object();",
+            "   }",
+            "}")
+        .expectOutput(
+            "package edu.ucr;",
+            "import javax.annotation.Nullable;",
+            "public class Test {",
+            "   Field foo1;",
+            "   Field foo2;",
+            "   @Nullable",
+            "   Field foo3, foo4;",
+            "   Object bar1(Object p1, Object p2) {",
+            "       return new Object();",
+            "   }",
+            "   Object bar2(Object p1, @Nullable Object p2) {",
+            "       return new Object();",
+            "   }",
+            "   Field foo5;",
+            "   @Nullable",
+            "   Object bar3(@Nullable Object p1, Object p2, Object p3, @Nullable Object p4) {",
+            "       return new Object();",
+            "   }",
+            "   Object bar4() {",
+            "       return new Object();",
+            "   }",
+            "}")
+        .addChanges(
+            new RemoveAnnotation(
+                new OnField("Test.java", "edu.ucr.Test", singleton("foo2")), annot),
+            new AddMarkerAnnotation(
+                new OnField("Test.java", "edu.ucr.Test", singleton("foo4")), annot),
+            new RemoveAnnotation(
+                new OnField("Test.java", "edu.ucr.Test", singleton("foo5")), annot),
+            new RemoveAnnotation(
+                new OnMethod("Test.java", "edu.ucr.Test", "bar1(Object, Object)"), annot),
+            new AddMarkerAnnotation(
+                new OnMethod("Test.java", "edu.ucr.Test", "bar3(Object, Object, Object, Object)"),
+                annot),
+            new RemoveAnnotation(new OnMethod("Test.java", "edu.ucr.Test", "bar4()"), annot),
+            new RemoveAnnotation(
+                new OnParameter("Test.java", "edu.ucr.Test", "bar1(Object, Object)", 0), annot),
+            new AddMarkerAnnotation(
+                new OnParameter("Test.java", "edu.ucr.Test", "bar2(Object, Object)", 1), annot),
+            new AddMarkerAnnotation(
+                new OnParameter(
+                    "Test.java", "edu.ucr.Test", "bar3(Object, Object, Object, Object)", 0),
+                annot),
+            new RemoveAnnotation(
+                new OnParameter(
+                    "Test.java", "edu.ucr.Test", "bar3(Object, Object, Object, Object)", 1),
+                annot),
+            new AddMarkerAnnotation(
+                new OnParameter(
+                    "Test.java", "edu.ucr.Test", "bar3(Object, Object, Object, Object)", 3),
+                annot))
+        .start();
+  }
 }
