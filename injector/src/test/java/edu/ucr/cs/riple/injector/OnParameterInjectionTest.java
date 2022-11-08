509aaae3d17204c92a4573aec5094b41669777dd
nimakarimipour <karimipour.nima@gmail.com>
2022-11-08 13:13:39 -0800

    add unit tests for method with multiple line decl

--- injector/src/test/java/edu/ucr/cs/riple/injector/OnParameterInjectionTest.java
+++ injector/src/test/java/edu/ucr/cs/riple/injector/OnParameterInjectionTest.java
@@ -274,4 +274,204 @@ public class OnParameterInjectionTest extends BaseInjectorTest {
                 "javax.annotation.Nullable"))
         .start();
   }
+
+  @Test
+  public void methodWithMultipleLineDeclarationFirstParam() {
+    injectorTestHelper
+        .addInput(
+            "Main.java",
+            "package edu.ucr;",
+            "public class Main {",
+            "   @Foo(clazz = String.class, value = \"Some description\")",
+            "   private static @Baz Object foo6(",
+            "       final @Baz Map<String, @Baz Object> m,",
+            "       @Foo(clazz = String.class, value = \"Some argument\") @Baz Object o1,",
+            "       Object o2,",
+            "       Object o3,",
+            "       @Foo(clazz = Object.class, value = \"Some other argument\") @Baz Object o4) {",
+            "     return new @Baz Runnable() {",
+            "       public void run() {",
+            "         System.out.print(\"log\");",
+            "       }",
+            "     };",
+            "   }",
+            "}")
+        .expectOutput(
+            "package edu.ucr;",
+            "import javax.annotation.Nullable;",
+            "public class Main {",
+            "   @Foo(clazz = String.class, value = \"Some description\")",
+            "   private static @Baz Object foo6(",
+            "       @Nullable final @Baz Map<String, @Baz Object> m,",
+            "       @Foo(clazz = String.class, value = \"Some argument\") @Baz Object o1,",
+            "       Object o2,",
+            "       Object o3,",
+            "       @Foo(clazz = Object.class, value = \"Some other argument\") @Baz Object o4) {",
+            "     return new @Baz Runnable() {",
+            "       public void run() {",
+            "         System.out.print(\"log\");",
+            "       }",
+            "     };",
+            "   }",
+            "}")
+        .addChanges(
+            new AddMarkerAnnotation(
+                new OnParameter(
+                    "Main.java",
+                    "edu.ucr.Main",
+                    "foo6(Map<String, Object>, Object, Object, Object, Object)",
+                    0),
+                "javax.annotation.Nullable"))
+        .start();
+  }
+
+  @Test
+  public void methodWithMultipleLineDeclarationSecondParam() {
+    injectorTestHelper
+        .addInput(
+            "Main.java",
+            "package edu.ucr;",
+            "public class Main {",
+            "   @Foo(clazz = String.class, value = \"Some description\")",
+            "   private static @Baz Object foo6(",
+            "       final @Baz Map<String, @Baz Object> m,",
+            "       @Foo(clazz = String.class, value = \"Some argument\") @Baz Object o1,",
+            "       Object o2,",
+            "       Object o3,",
+            "       @Foo(clazz = Object.class, value = \"Some other argument\") @Baz Object o4) {",
+            "     return new @Baz Runnable() {",
+            "       public void run() {",
+            "         System.out.print(\"log\");",
+            "       }",
+            "     };",
+            "   }",
+            "}")
+        .expectOutput(
+            "package edu.ucr;",
+            "import javax.annotation.Nullable;",
+            "public class Main {",
+            "   @Foo(clazz = String.class, value = \"Some description\")",
+            "   private static @Baz Object foo6(",
+            "       final @Baz Map<String, @Baz Object> m,",
+            "       @Nullable @Foo(clazz = String.class, value = \"Some argument\") @Baz Object o1,",
+            "       Object o2,",
+            "       Object o3,",
+            "       @Foo(clazz = Object.class, value = \"Some other argument\") @Baz Object o4) {",
+            "     return new @Baz Runnable() {",
+            "       public void run() {",
+            "         System.out.print(\"log\");",
+            "       }",
+            "     };",
+            "   }",
+            "}")
+        .addChanges(
+            new AddMarkerAnnotation(
+                new OnParameter(
+                    "Main.java",
+                    "edu.ucr.Main",
+                    "foo6(Map<String, Object>, Object, Object, Object, Object)",
+                    1),
+                "javax.annotation.Nullable"))
+        .start();
+  }
+
+  @Test
+  public void methodWithMultipleLineDeclarationThirdParam() {
+    injectorTestHelper
+        .addInput(
+            "Main.java",
+            "package edu.ucr;",
+            "public class Main {",
+            "   @Foo(clazz = String.class, value = \"Some description\")",
+            "   private static @Baz Object foo6(",
+            "       final @Baz Map<String, @Baz Object> m,",
+            "       @Foo(clazz = String.class, value = \"Some argument\") @Baz Object o1,",
+            "       Object o2,",
+            "       Object o3,",
+            "       @Foo(clazz = Object.class, value = \"Some other argument\") @Baz Object o4) {",
+            "     return new @Baz Runnable() {",
+            "       public void run() {",
+            "         System.out.print(\"log\");",
+            "       }",
+            "     };",
+            "   }",
+            "}")
+        .expectOutput(
+            "package edu.ucr;",
+            "import javax.annotation.Nullable;",
+            "public class Main {",
+            "   @Foo(clazz = String.class, value = \"Some description\")",
+            "   private static @Baz Object foo6(",
+            "       final @Baz Map<String, @Baz Object> m,",
+            "       @Foo(clazz = String.class, value = \"Some argument\") @Baz Object o1,",
+            "       @Nullable Object o2,",
+            "       Object o3,",
+            "       @Foo(clazz = Object.class, value = \"Some other argument\") @Baz Object o4) {",
+            "     return new @Baz Runnable() {",
+            "       public void run() {",
+            "         System.out.print(\"log\");",
+            "       }",
+            "     };",
+            "   }",
+            "}")
+        .addChanges(
+            new AddMarkerAnnotation(
+                new OnParameter(
+                    "Main.java",
+                    "edu.ucr.Main",
+                    "foo6(Map<String, Object>, Object, Object, Object, Object)",
+                    2),
+                "javax.annotation.Nullable"))
+        .start();
+  }
+
+  @Test
+  public void methodWithMultipleLineDeclarationFifthParam() {
+    injectorTestHelper
+        .addInput(
+            "Main.java",
+            "package edu.ucr;",
+            "public class Main {",
+            "   @Foo(clazz = String.class, value = \"Some description\")",
+            "   private static @Baz Object foo6(",
+            "       final @Baz Map<String, @Baz Object> m,",
+            "       @Foo(clazz = String.class, value = \"Some argument\") @Baz Object o1,",
+            "       Object o2,",
+            "       Object o3,",
+            "       @Foo(clazz = Object.class, value = \"Some other argument\") @Baz Object o4) {",
+            "     return new @Baz Runnable() {",
+            "       public void run() {",
+            "         System.out.print(\"log\");",
+            "       }",
+            "     };",
+            "   }",
+            "}")
+        .expectOutput(
+            "package edu.ucr;",
+            "import javax.annotation.Nullable;",
+            "public class Main {",
+            "   @Foo(clazz = String.class, value = \"Some description\")",
+            "   private static @Baz Object foo6(",
+            "       final @Baz Map<String, @Baz Object> m,",
+            "       @Foo(clazz = String.class, value = \"Some argument\") @Baz Object o1,",
+            "       Object o2,",
+            "       Object o3,",
+            "       @Nullable @Foo(clazz = Object.class, value = \"Some other argument\") @Baz Object o4) {",
+            "     return new @Baz Runnable() {",
+            "       public void run() {",
+            "         System.out.print(\"log\");",
+            "       }",
+            "     };",
+            "   }",
+            "}")
+        .addChanges(
+            new AddMarkerAnnotation(
+                new OnParameter(
+                    "Main.java",
+                    "edu.ucr.Main",
+                    "foo6(Map<String, Object>, Object, Object, Object, Object)",
+                    4),
+                "javax.annotation.Nullable"))
+        .start();
+  }
 }
