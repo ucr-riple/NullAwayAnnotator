43fa3761c5bfcd11ebe1781cfc8a696509bc41ab
nimakarimipour <karimipour.nima@gmail.com>
2022-12-02 15:56:02 -0600

    bug fix

--- annotator-core/src/main/java/edu/ucr/cs/riple/core/adapters/NullAwayV2Adapter.java
+++ annotator-core/src/main/java/edu/ucr/cs/riple/core/adapters/NullAwayV2Adapter.java
@@ -20,11 +20,12 @@ public class NullAwayV2Adapter extends NullAwayV1Adapter {
         "Expected 12 values to create Error instance in NullAway serialization version 2 but found: "
             + values.length);
     int offset = Integer.parseInt(values[4]);
+    String path = values[5].startsWith("file:/") ? values[5].substring(6) : values[5];
     return new Error(
         values[0],
         values[1],
         new Region(values[2], values[3]),
-        config.offSetHandler.translate(Paths.get(values[5]), offset),
+        config.offSetHandler.translate(Paths.get(path), offset),
         Location.createLocationFromArrayInfo(Arrays.copyOfRange(values, 6, 12)));
   }
 }
