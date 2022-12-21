235e0a6a4e9ffbce92cdb1ffb88951903ad70140
nimakarimipour <karimipour.nima@gmail.com>
2022-12-21 12:18:41 -0800

    updated header

--- annotator-scanner/src/test/java/edu/ucr/cs/riple/scanner/MethodCallTrackerTest.java
+++ annotator-scanner/src/test/java/edu/ucr/cs/riple/scanner/MethodCallTrackerTest.java
@@ -36,11 +36,19 @@ public class MethodCallTrackerTest extends AnnotatorScannerBaseTest<TrackerNodeD
 
   private static final DisplayFactory<TrackerNodeDisplay> METHOD_TRACKER_DISPLAY_FACTORY =
       values -> {
-        Preconditions.checkArgument(values.length == 4, "Expected to find 4 values on each line");
-        return new TrackerNodeDisplay(values[0], values[1], values[3], values[2]);
+        Preconditions.checkArgument(values.length == 5, "Expected to find 5 values on each line");
+        return new TrackerNodeDisplay(values[0], values[1], values[3], values[2], values[4]);
       };
   private static final String HEADER =
-      "REGION_CLASS" + '\t' + "REGION_MEMBER" + '\t' + "USED_MEMBER" + '\t' + "USED_CLASS";
+      "REGION_CLASS"
+          + '\t'
+          + "REGION_MEMBER"
+          + '\t'
+          + "USED_MEMBER"
+          + '\t'
+          + "USED_CLASS"
+          + '\t'
+          + "SOURCE_TYPE";
   private static final String FILE_NAME = "call_graph.tsv";
 
   public MethodCallTrackerTest() {
