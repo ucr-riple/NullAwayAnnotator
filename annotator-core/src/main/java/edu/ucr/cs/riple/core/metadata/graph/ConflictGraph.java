c0c374936215d01b3523e7ca6284b25217007555
Manu Sridharan <msridhar@gmail.com>
2022-12-27 16:39:27 -0800

    remove import

--- annotator-core/src/main/java/edu/ucr/cs/riple/core/metadata/graph/ConflictGraph.java
+++ annotator-core/src/main/java/edu/ucr/cs/riple/core/metadata/graph/ConflictGraph.java
@@ -35,7 +35,6 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 /**
