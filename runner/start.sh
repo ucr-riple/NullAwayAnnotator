ce47a5a511a242e30d63dfc1cc347e65db0664b6
nimakarimipour <karimipour.nima@gmail.com>
2023-01-16 11:31:31 -0800

    add docker

--- runner/start.sh
+++ runner/start.sh
@@ -1,9 +1,11 @@
 set -exu
 
-CURRENT_VERSION="annotator-core-1.3.6-alpha-2.jar"
+CURRENT_VERSION="annotator-core-1.3.6-alpha-4-SNAPSHOT.jar"
 PROJECT_ROOT=${PROJECT_ROOT:-$(git rev-parse --show-toplevel)}
 
 pushd "$PROJECT_ROOT"
+   rm -rvf runner/jars
+   mkdir runner/jars
    ./gradlew publishToMavenLocal --rerun-tasks
    mv annotator-core/build/libs/"$CURRENT_VERSION" runner/jars/core.jar
 
