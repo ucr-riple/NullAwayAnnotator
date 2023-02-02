c74f5270f5023f69c8a04bb26ae2702f1efdcd4c
nimakarimipour <karimipour.nima@gmail.com>
2023-02-01 18:03:42 -0800

    update

--- runner/start.sh
+++ runner/start.sh
@@ -6,7 +6,7 @@ CURRENT_VERSION="annotator-core-0.0.1.jar"
 pushd "/tmp/NullAwayAnnotator/"
    rm -rvf runner/jars
    mkdir runner/jars
-   ./gradlew publishToMavenLocal -x signMavenPublication -x signShadowPublication--rerun-tasks
+   ./gradlew publishToMavenLocal -x signMavenPublication -x signShadowPublication --rerun-tasks
    mv annotator-core/build/libs/"$CURRENT_VERSION" runner/jars/core.jar
 
    pushd runner
