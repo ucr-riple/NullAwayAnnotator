458a2a19081773bdaa2e47235f67bbfd53acce0f
nimakarimipour <karimipour.nima@gmail.com>
2023-02-08 11:53:10 -0800

    undo unwanted change

--- runner/start.sh
+++ runner/start.sh
@@ -1,14 +1,14 @@
 set -exu
 
-CURRENT_VERSION="annotator-core-1.3.6-SNAPSHOT.jar"
+CURRENT_VERSION="annotator-core-1.3.5.jar"
 PROJECT_ROOT=${PROJECT_ROOT:-$(git rev-parse --show-toplevel)}
 
 pushd "$PROJECT_ROOT"
    ./gradlew publishToMavenLocal --rerun-tasks
-   mv annotator-core/build/libs/"$CURRENT_VERSION" runner/jars/core.jar
+   mv core/build/libs/"$CURRENT_VERSION" runner/jars/core.jar
 
    pushd runner
-     pushd jars
+       pushd "$DIR"
          java -jar core.jar "$@"
        popd
      popd
