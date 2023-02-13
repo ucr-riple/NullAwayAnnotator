8a517d0afeb53cee66988c8434e43c0351cccd14
nimakarimipour <karimipour.nima@gmail.com>
2023-02-13 11:05:37 -0800

    prevent constructor @NullUnmarked for constructors

--- annotator-core/src/main/java/edu/ucr/cs/riple/core/Annotator.java
+++ annotator-core/src/main/java/edu/ucr/cs/riple/core/Annotator.java
@@ -238,7 +238,7 @@ public class Annotator {
             // find the corresponding method nodes.
             .map(
                 error -> {
-                  if (error.getRegion().isOnCallable()) {
+                  if (!error.isInitializationError() && error.getRegion().isOnCallable()) {
                     return methodDeclarationTree.findNode(error.encMember(), error.encClass());
                   }
                   // For methods invoked in an initialization region, where the error is that
@@ -274,8 +274,7 @@ public class Annotator {
                     return false;
                   }
                   // We can silence them by SuppressWarnings("NullAway.Init")
-                  return !error.messageType.equals("METHOD_NO_INIT")
-                      && !error.messageType.equals("FIELD_NO_INIT");
+                  return !error.isInitializationError();
                 })
             .map(
                 error ->
