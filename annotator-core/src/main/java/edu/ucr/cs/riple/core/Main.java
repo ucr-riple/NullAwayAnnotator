31b2a93d7e1c2b343085fa42db2d89017cf4feec
Nima Karimipour <karimipour.nima@gmail.com>
2023-07-11 10:07:45 -0700

    Save temp for now

--- annotator-core/src/main/java/edu/ucr/cs/riple/core/Main.java
+++ annotator-core/src/main/java/edu/ucr/cs/riple/core/Main.java
@@ -24,25 +24,59 @@
 
 package edu.ucr.cs.riple.core;
 
-import java.nio.file.Paths;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /** Starting point. */
 public class Main {
 
-  /**
-   * Starting point.
-   *
-   * @param args if flag '--path' is found, all configurations will be set up based on the given
-   *     json file, otherwise they will be set up according to the set of received cli arguments.
-   */
+  //  /**
+  //   * Starting point.
+  //   *
+  //   * @param args if flag '--path' is found, all configurations will be set up based on the given
+  //   *     json file, otherwise they will be set up according to the set of received cli
+  // arguments.
+  //   */
+  //  public static void main(String[] args) {
+  //    System.out.println("NEW VERSION");
+  //    Config config;
+  //    if (args.length == 2 && args[0].equals("--path")) {
+  //      config = new Config(Paths.get(args[1]));
+  //    } else {
+  //      config = new Config(args);
+  //    }
+  //    Annotator annotator = new Annotator(config);
+  //    annotator.start();
+  //  }
+
   public static void main(String[] args) {
-    System.out.println("NEW VERSION");
-    Config config;
-    if (args.length == 2 && args[0].equals("--path")) {
-      config = new Config(Paths.get(args[1]));
-    } else {
-      config = new Config(args);
+    final String dir = "/tmp/ucr-tainting/type-arg-test";
+
+    try (FileWriter writer = new FileWriter(dir + "/paths.tsv")) {
+      writer.write(
+          "/tmp/ucr-tainting/type-arg-test/taint.xml\t/tmp/ucr-tainting/type-arg-test/scanner.xml");
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+
+    List<String> newArgs = new ArrayList<>();
+    newArgs.addAll(List.of("-d", dir));
+    newArgs.addAll(
+        List.of("-bc", "cd /home/nima/Desktop/TestTypeArg && ./gradlew compileJava --rerun-tasks"));
+    newArgs.addAll(List.of("-cp", "/tmp/ucr-tainting/type-arg-test/paths.tsv"));
+    newArgs.addAll(List.of("-i", "edu.ucr.Initializer"));
+    newArgs.addAll(List.of("-n", "edu.ucr.cs.riple.taint.ucrtainting.qual.RUntainted"));
+    newArgs.addAll(List.of("-cn", "UCRTaint"));
+    newArgs.addAll(List.of("--depth", "1"));
+    // Uncomment to disable outer loop
+    newArgs.add("-dol");
+    // Uncomment to see build output
+    // newArgs.add("-rboserr");
+    // Uncomment to disable parallel processing
+    // newArgs.add("--disable-parallel-processing");
+    Config config;
+    config = new Config(newArgs.toArray(String[]::new));
     Annotator annotator = new Annotator(config);
     annotator.start();
   }
