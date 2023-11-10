f6a97bdc008cc80e58ede9e4c0c75025af74214e
nimakarimipour <karimipour.nima@gmail.com>
2023-11-09 22:03:49 -0800

    update dependency to 3.25.6

---
+++ injector/src/main/java/edu/ucr/cs/riple/injector/Main.java
@@ -0,0 +1,51 @@
+package edu.ucr.cs.riple.injector;
+
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.StaticJavaParser;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class Main {
+
+//    public static void main(String[] args) throws IOException {
+//        // find paths all java files in the given directory
+//        String directory = "/Users/nima/Developer/logisim-evolution/src/main/java/com/cburch";
+//        try(Stream<Path> paths = Files.walk(Paths.get(directory))){
+//      paths.forEach(
+//          new Consumer<Path>() {
+//            @Override
+//            public void accept(Path path) {
+//              if (path.toString().endsWith(".java")) {
+//                try {
+//                  StaticJavaParser.setConfiguration(
+//                      new ParserConfiguration()
+//                          .setLanguageLevel(ParserConfiguration.LanguageLevel.JAVA_17));
+//                  StaticJavaParser.parse(path.toFile());
+//                } catch (Exception e) {
+//                  System.out.println(path);
+////                  throw new RuntimeException(e);
+//                }
+//              }
+//            }
+//          });
+//        }
+//    }
+
+    public static void main(String[] args){
+        Path path = Paths.get("/Users/nima/Developer/logisim-evolution/src/main/java/com/cburch/logisim/tools/EditTool.java");
+        StaticJavaParser.setConfiguration(
+                      new ParserConfiguration()
+                          .setLanguageLevel(ParserConfiguration.LanguageLevel.JAVA_17));
+        try {
+            StaticJavaParser.parse(path.toFile());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
