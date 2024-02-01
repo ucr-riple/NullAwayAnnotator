5c95d427ab2843d4460ccbecd56d2ed2994f741d
Nima Karimipour <karimipour.nima@gmail.com>
2024-01-31 20:19:58 -0800

    Set language level to 17 in java parser and upgrade the dependency (#223)
    Fixes #88
    
    This PR sets parser configuration to `17` to enable Annotator to work on source code written in java 17.
    
    To support java 17 sources, we are only required to update java parser configuration, no change in `core` implementation is required. 
    
    Our default java version for publishing/compiling/testing is 11. This PR adds a CI job (named `parser-coinfiguration-17`) to specifically execute tests in `Java17Test` with java version 17. 
    
    
    Please note this PR also upgrades `javaparser` dependency to `3.25.7`.

--- annotator-core/src/main/java/edu/ucr/cs/riple/core/registries/field/FieldRegistry.java
+++ annotator-core/src/main/java/edu/ucr/cs/riple/core/registries/field/FieldRegistry.java
@@ -24,7 +24,6 @@
 
 package edu.ucr.cs.riple.core.registries.field;
 
-import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.BodyDeclaration;
@@ -36,13 +35,12 @@ import com.google.common.collect.Sets;
 import edu.ucr.cs.riple.core.module.ModuleConfiguration;
 import edu.ucr.cs.riple.core.registries.Registry;
 import edu.ucr.cs.riple.injector.Helper;
+import edu.ucr.cs.riple.injector.Injector;
 import edu.ucr.cs.riple.injector.exceptions.TargetClassNotFound;
 import edu.ucr.cs.riple.injector.location.Location;
 import edu.ucr.cs.riple.injector.location.OnClass;
 import edu.ucr.cs.riple.injector.location.OnField;
 import edu.ucr.cs.riple.scanner.Serializer;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Optional;
@@ -99,45 +97,41 @@ public class FieldRegistry extends Registry<ClassFieldRecord> {
       String clazz = values[0];
       // Path to class.
       Path path = Helper.deserializePath(values[1]);
-      CompilationUnit tree;
-      try {
-        tree = StaticJavaParser.parse(path);
-        NodeList<BodyDeclaration<?>> members;
-        try {
-          members = Helper.getTypeDeclarationMembersByFlatName(tree, clazz);
-        } catch (TargetClassNotFound notFound) {
-          System.err.println(notFound.getMessage());
-          return null;
-        }
-        ClassFieldRecord record = new ClassFieldRecord(path, clazz);
-        members.forEach(
-            bodyDeclaration ->
-                bodyDeclaration.ifFieldDeclaration(
-                    fieldDeclaration -> {
-                      NodeList<VariableDeclarator> vars = fieldDeclaration.getVariables();
-                      if (vars.getFirst().isEmpty()) {
-                        // unexpected but just in case.
-                        return;
-                      }
-                      record.addFieldDeclaration(fieldDeclaration);
-                      // Collect uninitialized fields at declaration.
-                      vars.forEach(
-                          variableDeclarator -> {
-                            String fieldName = variableDeclarator.getNameAsString();
-                            if (variableDeclarator.getInitializer().isEmpty()) {
-                              uninitializedFields.put(clazz, fieldName);
-                            }
-                          });
-                    }));
-        // We still want to keep the information about the class even if it has no field
-        // declarations, so we can retrieve tha path to the file from the given class flat name.
-        // This information is used in adding suppression annotations on class level.
-        return record;
-      } catch (FileNotFoundException e) {
+      CompilationUnit tree = Injector.parse(path);
+      if (tree == null) {
         return null;
-      } catch (IOException e) {
-        throw new RuntimeException(e);
       }
+      NodeList<BodyDeclaration<?>> members;
+      try {
+        members = Helper.getTypeDeclarationMembersByFlatName(tree, clazz);
+      } catch (TargetClassNotFound notFound) {
+        System.err.println(notFound.getMessage());
+        return null;
+      }
+      ClassFieldRecord record = new ClassFieldRecord(path, clazz);
+      members.forEach(
+          bodyDeclaration ->
+              bodyDeclaration.ifFieldDeclaration(
+                  fieldDeclaration -> {
+                    NodeList<VariableDeclarator> vars = fieldDeclaration.getVariables();
+                    if (vars.getFirst().isEmpty()) {
+                      // unexpected but just in case.
+                      return;
+                    }
+                    record.addFieldDeclaration(fieldDeclaration);
+                    // Collect uninitialized fields at declaration.
+                    vars.forEach(
+                        variableDeclarator -> {
+                          String fieldName = variableDeclarator.getNameAsString();
+                          if (variableDeclarator.getInitializer().isEmpty()) {
+                            uninitializedFields.put(clazz, fieldName);
+                          }
+                        });
+                  }));
+      // We still want to keep the information about the class even if it has no field
+      // declarations, so we can retrieve tha path to the file from the given class flat name.
+      // This information is used in adding suppression annotations on class level.
+      return record;
     };
   }
 
