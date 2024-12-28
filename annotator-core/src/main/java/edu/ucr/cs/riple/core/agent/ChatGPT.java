4502cc7b2b9c34d9cab590e982ae635d935f1c1d
nimakarimipour <karimipour.nima@gmail.com>
2024-12-27 23:18:55 -0800

    update

--- annotator-core/src/main/java/edu/ucr/cs/riple/core/agent/ChatGPT.java
+++ annotator-core/src/main/java/edu/ucr/cs/riple/core/agent/ChatGPT.java
@@ -6,19 +6,29 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Objects;
 
 public class ChatGPT {
 
     private static final String MODEL = "gpt-4o";
-    private final String apiKey = "";
+    private final String apiKey;
 
     public ChatGPT() {
-
+        // read openai-api-key.txt from resources
+        try {
+            // check if the file is in the resources
+            if(getClass().getResource("/openai-api-key.txt") == null){
+                throw new RuntimeException("openai-api-key.txt not found in resources");
+            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream("/openai-api-key.txt"))));
+            apiKey = reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String ask(String prompt){
         String url = "https://api.openai.com/v1/chat/completions";
-        String model = "gpt-3.5-turbo";
 
         try {
             URL obj = new URL(url);
@@ -28,7 +38,7 @@ public class ChatGPT {
             connection.setRequestProperty("Content-Type", "application/json");
 
             // The request body
-            String body = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
+            String body = "{\"model\": \"" + MODEL + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
             connection.setDoOutput(true);
             OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
             writer.write(body);
@@ -39,14 +49,11 @@ public class ChatGPT {
             BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line;
 
-            StringBuffer response = new StringBuffer();
-
+            StringBuilder response = new StringBuilder();
             while ((line = br.readLine()) != null) {
                 response.append(line);
             }
             br.close();
-
-            // calls the method to extract the message.
             return extractMessageFromJSONResponse(response.toString());
 
         } catch (IOException e) {
@@ -56,9 +63,7 @@ public class ChatGPT {
 
     public static String extractMessageFromJSONResponse(String response) {
         int start = response.indexOf("content")+ 11;
-
         int end = response.indexOf("\"", start);
-
         return response.substring(start, end);
 
     }
